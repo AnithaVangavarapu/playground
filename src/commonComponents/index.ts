2b2708398371224a3889291a8232eb1699b64245
AnithaVangavarapu <anitha.vangavarapu@quadone.com>
2025-05-07 12:51:52 +0530

    fix isuue

--- src/commonComponents/index.ts
+++ src/commonComponents/index.ts
@@ -2,4 +2,3 @@ export { default as Text } from "./Text";
 export { default as TextArea } from "./TextArea";
 export { default as Dropdown } from "./Dropdown";
 export { default as Date } from "./Date";
-export { default as Time } from "./Time";
