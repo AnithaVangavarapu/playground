a42ca17e0aaf76c5cd749be52b8271ace519b771
AnithaVangavarapu <anitha.vangavarapu@quadone.com>
2025-05-07 12:50:18 +0530

    fix deploy error

--- src/pages/Playground/studyDrugDoseDiary/SDDD.tsx
+++ src/pages/Playground/studyDrugDoseDiary/SDDD.tsx
@@ -1,10 +1,6 @@
 import GenerateQR from "../../../components/GenerateQR";
 
 const SDDD = () => {
-  const handleChange = (val: string) => {
-    console.log(val);
-  };
-
   return (
     <div>
       <GenerateQR />
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,11 @@
 import { useSignIn } from "./useSignIn";
 const SignIn = () => {
   const { handleLogin } = useSignIn();
-  return <div onClick={handleLogin}>SignIn</div>;
+  return (
+    <div onClick={handleLogin} className="border">
+      SignIn
+    </div>
+  );
 };
 
 export default SignIn;
